27f44f0a9bf7359d635bf0a7fb07e83f45db3ae3
AidenCM1234 <aidenm124@nycstudents.net>
2025-09-10 06:20:21 -0700

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from turtle import *
 t = Turtle()
 t.shape('turtle')
 
-def square(x):
+"""def square(x):
     t.forward(x)
     t.left(90)
     t.forward(x)
@@ -29,5 +29,42 @@ def right():
     t.forward(142)
 right()
 
+def rec(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x+250)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x+250)
+    t.left(90)
+rec(200)
 
-turtle.done
+for i in range(3):
+    print(i)"""
+
+"""t.speed(2000)
+for i in range(3):
+    t.forward(1000)
+    t.left(100)
+    t.forward(500)
+    t.right(50)"""
+idelength = 100
+rotate = 90
+def square(x,y):
+    for i in range(4):
+        t.forward(x)
+        t.left(y)
+square(100,90)
+def doubleSquares(iRange):
+    length = 25
+    for i in range(iRange):
+        square(length, 90)
+        length = length * 2
+doubleSquares(5)
+def addSquares(iRange):
+    length = 25
+    for i in range(iRange):
+        square(length, 90)
+        length + 5
+addSquares(5)
